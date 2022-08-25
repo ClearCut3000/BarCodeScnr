6bee8de37b4a84501fabaa60317b18e234075826
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-25 19:26:15 +0300

    Error handling in ScannerVC

--- BarCodeScnr/ScannerVC.swift
+++ BarCodeScnr/ScannerVC.swift
@@ -8,8 +8,18 @@
 import AVFoundation
 import UIKit
 
+enum CameraError: String {
+  case invalidDevice = "Unable to permit video device."
+  case invalidDeviceInput = "Unable to capture the input."
+  case cantAddInput = "Can’t add an input to a capture session."
+  case cantAddOutput = "A given output can be added to the session."
+  case previewLayerError = "Error with a layer that displays the video as it’s captured."
+  case invalidScanedValue = "Unadle to find EAN-8 or EAN-13 codes."
+}
+
 protocol ScannerVCDelegate: AnyObject {
   func didFind(barcode: String)
+  func didSurface(error: CameraError)
 }
 
 final class ScannerVC: UIViewController {
@@ -32,31 +42,41 @@ final class ScannerVC: UIViewController {
   //MARK: - Methods
   private func setupCaptureSession() {
     guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else {
+      scannerDelegate.didSurface(error: CameraError.invalidDevice)
       return
     }
+
     let videoInput: AVCaptureDeviceInput
     do {
       try videoInput = AVCaptureDeviceInput(device: videoCaptureDevice)
     } catch {
+      scannerDelegate.didSurface(error: CameraError.invalidDeviceInput)
       return
     }
+
     if captureSession.canAddInput(videoInput) {
       captureSession.addInput(videoInput)
     } else {
+      scannerDelegate.didSurface(error: CameraError.cantAddInput)
       return
     }
+
     let metaDataOutput = AVCaptureMetadataOutput()
     if captureSession.canAddOutput(metaDataOutput) {
       captureSession.addOutput(metaDataOutput)
       metaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
       metaDataOutput.metadataObjectTypes = [.ean13, .ean8]
     } else {
+      scannerDelegate.didSurface(error: CameraError.cantAddOutput)
       return
     }
+
     previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
     guard let previewLayer = previewLayer else {
+      scannerDelegate.didSurface(error: CameraError.previewLayerError)
       return
     }
+
     previewLayer.videoGravity = .resizeAspectFill
     view.layer.addSublayer(previewLayer)
     captureSession.startRunning()
@@ -67,12 +87,15 @@ final class ScannerVC: UIViewController {
 extension ScannerVC: AVCaptureMetadataOutputObjectsDelegate {
   func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
     guard let object = metadataObjects.first else {
+      scannerDelegate.didSurface(error: CameraError.invalidScanedValue)
       return
     }
     guard let machineReadableObject = object as? AVMetadataMachineReadableCodeObject else {
+      scannerDelegate.didSurface(error: CameraError.invalidScanedValue)
       return
     }
     guard let barcode = machineReadableObject.stringValue else {
+      scannerDelegate.didSurface(error: CameraError.invalidScanedValue)
       return
     }
     scannerDelegate.didFind(barcode: barcode)
