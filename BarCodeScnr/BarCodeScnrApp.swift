34b1454bcd33f783f1916e58cd151b2ff98ec9dd
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-08-25 17:04:12 +0300

    Initial Commit

---
+++ BarCodeScnr/BarCodeScnrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCodeScnrApp.swift
+//  BarCodeScnr
+//
+//  Created by Николай Никитин on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct BarCodeScnrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
